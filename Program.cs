71427fc63b0b6134329352926ad50acc3bde2c6f
Camiosha <camiosha@gmail.com>
2019-06-06 00:26:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            short x = 8, y = 5;
+            Console.WriteLine("x=8, y=5");
+            
+            if (x == y)
+                Console.WriteLine("x == y true");
+            else Console.WriteLine("x == y false");
+
+            if (x != y)
+                Console.WriteLine("x != y true");
+            else Console.WriteLine("x != y false");
+
+            if (x > y)
+                Console.WriteLine("x > y true");
+            else Console.WriteLine("x > y false");
+
+            if (x < y)
+                Console.WriteLine("x < y true");
+            else Console.WriteLine("x < y false");
+
+            if (x >= y)
+                Console.WriteLine("x >= y true");
+            else Console.WriteLine("x >= y false");
+
+            if (x <= y)
+                Console.WriteLine("x <= y true");
+            else Console.WriteLine("x <= y false");
+        }
+    }
+}
